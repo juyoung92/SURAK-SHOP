dcbd1e30b090030877c0492b48fb2ffe927cfca4
ju0 <suraksan92@gmail.com>
2023-04-05 20:11:23 +0900

    기초 해더 마크업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='header'>
+            <div className='h-top'>
+                <div className='logo'>SURAKSAN</div>
+                <button className="menu material-icons" type='button'>menu</button>
+            </div>
+
+        </div>
     </div>
   );
 }
